ee42c5d3a74e90467d9101b703b5a87f6fae1e81
sevanamuradova3 <146056669+sevanamuradova3@users.noreply.github.com>
2023-10-20 04:46:04 +0400

    if

---
+++ index.js
@@ -0,0 +1,7 @@
+let a=6;
+        let b=4;
+        if (a>b)
+         { console.log("a b-den boyukdur");}
+         else if(a === b)
+          { console.log("a b-ye beraberdir");}
+          else {console.log("a b-den kicikdir");}
